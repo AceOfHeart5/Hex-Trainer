97557eed0f84f6888eda6f5a28ba194d52439cdd
AceOfHeart5 <evanconwaymusic@gmail.com>
2019-02-06 22:43:59 -0500

    Repo Fixed
    Github is weird. When I try to make a new repo in a location, it creates a folder inside that location, then puts the git info in there. I have to manually put all the git info in the new folder into the parent folder (where the project files are), and then tell git the path again. Frustrating.

---
+++ src/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+    private Scanner keyboard = new Scanner(System.in);
+    private String input;
+
+    private final String[] hex = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+    private final int[] binary = {0000, 0001, 0010, 0011, 0100, 0101, 0110, 0111,
+            1000, 1001, 1010, 1011, 1100, 1101, 1111};
+
+    public static void main(String[] args) {
+
+    }
+}
